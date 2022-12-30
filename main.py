c5871353e42d8d40e358a133bdbaec9d18a9ee9e
Archer <heffernarcher@gmail.com>
2022-12-30 18:59:50 -0500

    grabbed the correct part of the html file, fixed structure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 START = 9219
-END = 9327
+# END = 9327
+END = START + 1
 OUTPUT_FILE = 'output.txt'
 BASE_URL = 'https://apps.tamidgroup.org/Consulting/Company/posting?id='
 
@@ -12,20 +13,36 @@ def main():
         for i in range(START, END + 1):
             html = get_html(i)
             company:dict = get_content(i, html)
-            if company.has_key('name') and company.name not in companies:
+            if 'name' in company.keys() and company['name'] not in companies:
                 print_to_output_file(company, f)
-            companies.add(company.name.lower())
+            companies.add(company['name'].lower())
 
 def get_html(i: int) -> str:
-    f = open('test.html', 'r')
+    f = open('uh.html', 'r')
     return f.read()
 
 
-def get_content(i: int, html_file) -> dict:
+def get_content(id: int, html_file) -> dict:
     content = dict()
 
     soup = BeautifulSoup(html_file, 'lxml')
-    soup.find('div', class_='row')
+
+    box = soup.find_all('div', class_= 'u-shadow-v11 rounded g-pa-30')
+    list_group_items = box[0].find_all('div', class_= 'list-group-item')
+
+    content['name'] = ''
+    content['industry'] = ''
+    content['url'] = f'{BASE_URL}{id}'
+    content['website'] = ''
+    content['company_description'] = ''
+    content['project_description'] = ''
+    content['deliverable'] = ''
+    content['work_time'] = ''
+    content['new_or_existing'] = ''
+    content['deliverable_type'] = ''
+    content['work_type'] = ''
+    content['tech_stack'] = ''
+    return content
 
 def print_to_output_file(content: dict, f):
     """Takes dict as input and writes the content to output file"""
