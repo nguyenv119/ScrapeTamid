cc8191a057ff1f8a6ce5054cd41425d6934d5f2e
ArcherHeffern <heffernarcher@gmail.com>
2024-01-15 14:40:00 -0500

    revamped interface

--- README.md
+++ README.md
@@ -16,43 +16,12 @@ Run the following commands in bash / zsh / wsl (pretty much anything but powersh
   pip install -r requirements.txt # or pip3.10 / pip3 if this gives you an error
 ```
 
-2. Configure settings
-
-Since you have to be authenticated to see projects, put your credentials in a .env file as such
-
-``` bash
-#### .env ####
-email="youremail"
-password="yourpassword"
-```
-
-Update the start and end page Ids you would like to scan in config.py
-You can get a good sense of what numbers are good by checking the projects you can see on the dashboard, and check their ids in the url. 
-
-Eg. https://apps.tamidgroup.org/Consulting/Company/posting?id=11397 has id 11397. Find the max and min values and then have it range around 2000 projects.
-
-``` python
-#### config.py ####
-config = Config(
-    # Edit start and end here, and email / password in .env
-    start=9000,
-    end=11000,
-    ...
-)
- ```
-
 ### Running the script
-Assuming you did everything correctly, you can now run the script
-
 ``` bash
-python scraper.py
+python scraper.py <output_file> <email> <password> <start_id> <end_id>
 ```
-
-You will be prompted for if you would like to scrape consulting or tech consulting, and then
-for the name of the output file. Just give it a name without a file extension
 
 Once the script has run, you can view the html file by opening it in your file explorer, right clicking, and opening with chrome/firefox/safari.
 
-### Notes: 
- - To avoid getting rate limited, a request is sent every .5 seconds, this means to scan all 2000 possible projects in the default range,
- - this will take ~16.6 repeating minutes. Although this seems like a long time, it pales in comparison to trying to reroll, especially since you aren't guarenteed to find all projects. 
+### Options
+For additional options and help, run `python scraper.py -help`
--- scraper.py
+++ scraper.py
@@ -1,47 +1,96 @@
 from bs4 import BeautifulSoup
+import argparse
+from dataclasses import dataclass
+from typing import Optional
+from argparse import Namespace
 import requests
 import time
 from scrapeConsulting import get_consulting_content
 from scrapeTech import get_tech_content
-from config import config
-
 
 payload = {
-  'Email': config.email, 
-  'password': config.password,
-  'submit': 'Sign in',
-  '__EVENTTARGET': '',
-  '__EVENTARGUMENT': ''
+    'Email': None,
+    'password': None,
+    'submit': 'Sign in',
+    '__EVENTTARGET': '',
+    '__EVENTARGUMENT': ''
 }
 
+
+@dataclass
+class Config:
+    start: int
+    end: int
+    email: str
+    password: str
+    category: str
+    out: str
+    delay: float
+    debug: bool = False
+    test: Optional[int] = None
+    base_url: str = 'https://apps.tamidgroup.org/Consulting/Company/posting?id='
+    login_url = 'https://apps.tamidgroup.org/login'
+
+
+def parse_args() -> Config:
+    parser = argparse.ArgumentParser(
+        prog='tamid scraper',
+        description='A webscraper to view all of tamids groups projects as one of their PMs in an easily digestable html file',
+        epilog="""
+            It is up to you to figure out what range to scan. I would suggest looking at the url of the projects offered and make an estimate. A range of 2000 is sufficient. The output file is in html format
+        """)
+    parser.add_argument('-c', '--category',
+                        choices=['tech', 'consulting'], default='tech', help='Choose track to scrape')
+    parser.add_argument('out', help='Output file name')
+    parser.add_argument('email', help='Tamid Platform email')
+    parser.add_argument('password', help='Tamid Platform password')
+    parser.add_argument(
+        'start', type=int, help='Starting index')
+    parser.add_argument('end', type=int, help='Ending index')
+    parser.add_argument('-d', '--delay', type=int, default=.5,
+                        help='Delay to prevent being ratelimited')
+    args = parser.parse_args()
+
+    config = Config(
+        start=args.start,
+        end=args.end,
+        email=args.email,
+        password=args.password,
+        category=args.category,
+        out=args.out,
+        delay=args.delay
+    )
+    payload['email'] = config.email
+    payload['password'] = config.password
+    return config
+
+
 def main():
-  try: 
-    which = int(input("Consulting(0)\t\tTech(1)\t\tExit(^c): "))
-    output = input("Output file name: ") + ".html"
-    if which:
-      scraper(get_tech_content, output)
-    else: 
-      scraper(get_consulting_content, output)
-  except KeyboardInterrupt:
-    print("\nExiting")
-    return
+    config = parse_args()
+    try:
+        if config.category == 'tech':
+            scraper(config, get_tech_content)
+        else:
+            scraper(config, get_consulting_content)
+    except KeyboardInterrupt:
+        print("\nExiting")
 
 
-def scraper(scraper_function, output_file):
+def scraper(config: Config, scraper_function):
 
-  valid_count = 0
+    valid_count = 0
 
-  with open(output_file, 'w') as f:
-    with requests.Session() as s:
-      start_time = time.time()
+    with open(config.out, 'w') as f:
+        with requests.Session() as s:
+            start_time = time.time()
 
-          # login
-      if not login(config.login_url, payload, s):
-        print('authetication error')
-        return
-      print("Logged in")
+            # login
+            if not login(config.login_url, payload, s):
+                print('authetication error')
+                return
+            print("Logged in")
 
-      f.write("""<!DOCTYPE html>
+            f.write("""<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -51,56 +100,58 @@ def scraper(scraper_function, output_file):
 </head>
 <body>""")
 
-    for i in range(config.start, config.end + 1):
-      print(f"{i - config.start + 1}/{config.end - config.start + 1}", end="")
-      internal_start = time.time()
-      html = s.get(config.base_url + str(i))
-      html = html.text
-      company = scraper_function(i, html, config.base_url)
-      if company:
-          print_to_output_file(company, f)
-          valid_count += 1
-      internal_end = time.time()
-      time.sleep(max(0, config.delay - (internal_end - internal_start)))
+        for i in range(config.start, config.end + 1):
+            print(f"{i - config.start + 1}/{config.end - config.start + 1}", end="")
+            internal_start = time.time()
+            html = s.get(config.base_url + str(i))
+            html = html.text
+            company = scraper_function(i, html, config.base_url)
+            if company:
+                print_to_output_file(company, f)
+                valid_count += 1
+            internal_end = time.time()
+            time.sleep(max(0, config.delay - (internal_end - internal_start)))
 
-      # Stats
-      f.write("</body></html>")
-      if config.debug:
-        total_time = time.time() - start_time
-        print(f"Complete\nRuntime: {total_time}\nRuntime minus delay: {total_time - config.delay * (config.end - config.start)}\nValid items: {valid_count}")
+            # Stats
+            f.write("</body></html>")
+            if config.debug:
+                total_time = time.time() - start_time
+                print(
+                    f"Complete\nRuntime: {total_time}\nRuntime minus delay: {total_time - config.delay * (config.end - config.start)}\nValid items: {valid_count}")
 
 
 def login(url, payload, session):
-  """Returns boolean if logging in was successful"""
-  page = session.get(url)
-  soup = BeautifulSoup(page.content, 'lxml')
-  viewStateElement = soup.select_one("#__VIEWSTATE")
-  viewStateGeneratorElement = soup.select_one("#__VIEWSTATEGENERATOR")
-  eventValidationElement = soup.select_one("#__EVENTVALIDATION")
-  if not viewStateElement or not viewStateGeneratorElement or not eventValidationElement:
-    raise Exception("Could not find __VIEWSTATE, __VIEWSTATEGENERATOR, or __EVENTVALIDATION \
+    """Returns boolean if logging in was successful"""
+    page = session.get(url)
+    soup = BeautifulSoup(page.content, 'lxml')
+    viewStateElement = soup.select_one("#__VIEWSTATE")
+    viewStateGeneratorElement = soup.select_one("#__VIEWSTATEGENERATOR")
+    eventValidationElement = soup.select_one("#__EVENTVALIDATION")
+    if not viewStateElement or not viewStateGeneratorElement or not eventValidationElement:
+        raise Exception("Could not find __VIEWSTATE, __VIEWSTATEGENERATOR, or __EVENTVALIDATION \
                       This could imply the login page has changed")
-  payload["__VIEWSTATE"] = viewStateElement["value"]
-  payload["__VIEWSTATEGENERATOR"] = viewStateGeneratorElement["value"]
-  payload["__EVENTVALIDATION"] = eventValidationElement["value"] 
+    payload["__VIEWSTATE"] = viewStateElement["value"]
+    payload["__VIEWSTATEGENERATOR"] = viewStateGeneratorElement["value"]
+    payload["__EVENTVALIDATION"] = eventValidationElement["value"]
 
-  session.post(url, data=payload)
+    session.post(url, data=payload)
 
-  open_page = session.get("https://apps.tamidgroup.org/Consulting/PMPD/ConsultingDashboard")
+    open_page = session.get(
+        "https://apps.tamidgroup.org/Consulting/PMPD/ConsultingDashboard")
 
-  # kinda hacky but it works
-  return not page.text[:1000] == open_page.text[:1000]
+    # kinda hacky but it works
+    return not page.text[:1000] == open_page.text[:1000]
 
 
 def print_to_output_file(content: dict, f):
-  """Takes dict as input and writes the content to output file"""
-  for key, value in content.items():
-    if key == "url" or key == "website":
-      f.write(f"<div><b>{key}</b>: <a href={value}>{value}</a><br><br>")
-    else:
-      f.write(f"<div><b>{key}</b>: {value}</div><br>")
-  f.write("<hr><br>")
+    """Takes dict as input and writes the content to output file"""
+    for key, value in content.items():
+        if key == "url" or key == "website":
+            f.write(f"<div><b>{key}</b>: <a href={value}>{value}</a><br><br>")
+        else:
+            f.write(f"<div><b>{key}</b>: {value}</div><br>")
+    f.write("<hr><br>")
 
 
 if __name__ == '__main__':
-  main()
+    main()
